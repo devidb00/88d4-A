dafc53230f2ca215cde9341db1ed9eec551d6ad5
Tarik ID BELLOUCH <tarikidb.dev@gmail.com>
2024-01-03 13:10:21 +0100

    env file | new way

--- app/server/app.py
+++ app/server/app.py
@@ -1,28 +1,30 @@
 from fastapi import FastAPI
 from server.config import settings
 from server.database import MongoDB
-
+from contextlib import asynccontextmanager
 # routers
 from server.routes.account import router as AccountRouter
-app = FastAPI()
 
-# routers path (include)
-app.include_router(AccountRouter, tags=['User'], prefix="/api/user")
-
-@app.on_event("startup")
-def startup_db_client():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     # Database connection
-    db_name = settings.model_config.get('MONGODB_NAME')
-    db_username = settings.model_config.get('MONGODB_USERNAME')
-    db_password = settings.model_config.get('MONGODB_PASSWORD')
+    db_name = settings.mongodb_name
+    db_username = settings.mongodb_username
+    db_password = settings.mongodb_password
 
     try:
         app.mongodb_client = MongoDB(db_username, db_password)
         app.database = app.mongodb_client.connect(db_name)
-        print("Connected to the MongoDB database!")
-    except:
-        print("Connection failed. Check DB credentials!")
 
-@app.on_event("shutdown")
-def shutdown_db_client():
-    app.mongodb_client.close()
+        print("Connected to the MongoDB database!")
+    except Exception as e:
+        print(e)
+
+    yield
+    print("Connection failed. Check DB credentials!")
+
+app = FastAPI(lifespan=lifespan)
+
+# routers path
+app.include_router(AccountRouter, tags=['User'], prefix="/api/user")
+
--- app/server/config.py
+++ app/server/config.py
@@ -4,6 +4,6 @@ class Settings(BaseSettings):
     app_name: str = "88d4-A"
     items_per_user: int = 50
     admin_email: str = "tarikidb.dev@gmail.com"
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra='allow', env_file_encoding='utf-8')
 
 settings = Settings()

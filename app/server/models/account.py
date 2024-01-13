693717c3b1340f1fc57aa0d44b2a664104be6e2a
Tarik ID BELLOUCH <tarikidb.dev@gmail.com>
2024-01-13 20:23:55 +0100

    env file | new way

--- app/server/models/account.py
+++ app/server/models/account.py
@@ -5,3 +5,7 @@ from server.database import mongodb_db
 def find_account(id):
     collection = mongodb_db.db.get_collection('Menu')
     return dumps(collection.find({ '_id': ObjectId(id) }))
+
+def insert_account(account):
+    collection = mongodb_db.db.get_collection('Menu')
+    collection.insert_one(account)
--- app/server/routes/account.py
+++ app/server/routes/account.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from fastapi import APIRouter, Response
-from server.models.account import find_account
+from server.models.account import find_account, insert_account
 
 router = APIRouter()
 
@@ -12,3 +12,8 @@ class Account(BaseModel):
 @router.get('/')
 def account_root(id: str):
     return Response(content=find_account(id), media_type="application/json", status_code=200)
+
+@router.post('/')
+def add_account(account: Account):
+    insert_account(account.dict())
+    return Response(status_code=204)

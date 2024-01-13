998102ea531bd3d8a90d399fee5cb52c82e8e277
Tarik ID BELLOUCH <tarikidb.dev@gmail.com>
2024-01-13 20:19:16 +0100

    env file | new way

--- app/server/schemas/account.py
+++
@@ -1,6 +0,0 @@
-from mongoengine import *
-
-class AccountSchema(Document):
-    name: str     = StringField(required=True)
-    email: str    = StringField(required=True)
-    password: str = StringField(required=True)
